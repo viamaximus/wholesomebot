011d4a52889a4174ab079dde14a9001ddaf0d487
max <ianawestlake@gmail.com>
2024-05-02 15:48:51 -0400

    set up status

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import os
 import discord
 from dotenv import load_dotenv
 from discord import Intents, Client, Message
-from discord.ext import commands
+from discord.ext import commands, tasks
 from responses import get_response
 import random
+from itertools import cycle
 
 #load token
 load_dotenv()
 TOKEN: Final[str] = os.getenv('DISCORD_TOKEN')
 
-#bot setup
-intents = Intents.default()
-intents.message_content = True
-#client: Client = Client(intents=intents)
-
 client = commands.Bot(command_prefix='~', intents=discord.Intents.all())
+
+bot_status = cycle(['in my banky', ':3:3:#:#', 'mphghh...', 'in your walls'])
+@tasks.loop(seconds=10)                   
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(bot_status)))
+
+
+#handle startup
+@client.event
+async def on_ready() -> None:
+    print(f'{client.user} is now running!')
+    change_status.start()
+
+
 
 @client.command(alias=['8ball', 'eightball', '8 ball', 'eight ball'])
 async def magic8ball(ctx, *, question):
@@ -31,6 +41,7 @@ async def ping(ctx):
     await ctx.send(f"Bot latency: {bot_latency}ms")
 
 #message functionality
+
 async def send_message(message: Message, user_message: str) -> None:
     if not user_message: 
         print('(No message to send because intents were not enabled)')
@@ -44,11 +55,6 @@ async def send_message(message: Message, user_message: str) -> None:
         await message.author.send(response) if is_private else await message.channel.send(response)
     except Exception as e: 
         print(e)
-
-#handle startup
-@client.event
-async def on_ready() -> None:
-    print(f'{client.user} is now running!')
 
 #handle incoming messages
 @client.event
