ba9c87eb89ccc6f37298718b09fbce11ada6b867
max <ianawestlake@gmail.com>
2024-05-02 16:05:48 -0400

    removed old message format

--- main.py
+++ main.py
@@ -54,23 +54,6 @@ async def ping(ctx):
     bot_latency = round(client.latency * 1000)
     await ctx.send(f"Bot latency: {bot_latency}ms")
 
-
-
-#message functionality
-async def send_message(message: Message, user_message: str) -> None:
-    if not user_message: 
-        print('(No message to send because intents were not enabled)')
-        return
-    #is_private = user_message[0] == '?'
-
-    if is_private := user_message[0] == '?': 
-        user_message = user_message[1:]
-    try: 
-        response: str = get_response(user_message)
-        await message.author.send(response) if is_private else await message.channel.send(response)
-    except Exception as e: 
-        print(e)
-
 #handle incoming messages
 @client.event
 async def on_message(message: Message) -> None:
