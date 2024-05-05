be321e8242afda8aeadc97293b90dddf8d4c0ea7
max <ianawestlake@gmail.com>
2024-05-05 02:33:08 -0400

    updated infection.py to include list of keywords, stored in userdata/.keywords

--- cogs/Infection.py
+++ cogs/Infection.py
@@ -12,10 +12,15 @@ class Infection(commands.Cog):
         #self.scheduler.add_job(self.update_infections, 'cron', hour=19)  # Infect one member daily at 7 PM
         self.scheduler.start()
 
+    def load_keywords(self):
+        with open('userdata/.keywords', 'r') as file:
+            keywords = [line.strip() for line in file.readlines()]
+        return keywords
 
     #triggered when a message is detected
     @commands.Cog.listener()
     async def on_message(self, message):
+        
         print(f"Message from user {message.author} detected: {message.content}")
         if message.author.bot or not self.data_handler: # If the message author is a bot or the data handler is not available
             print('Ignoring message, bot detected, or data not in handler')
@@ -28,22 +33,27 @@ class Infection(commands.Cog):
             print(f"Member {message.author.name} not found in data")
             return
         
-        print(f"message author passed checks")
+        #print(f"message author passed checks")
         
-        if members_data[member_id]['exposure_status'] in ['exposed', 'infected']:
-            print(f"Member {message.author.name} is exposed or infected.")
+        # Check if the message contains any of the keywords
+        if any(keyword in message.content for keyword in self.load_keywords()):
             members_data[member_id]['exposure_score'] += 1 # Increment the exposure score by 1
-            print(f"incremented exposure score for {message.author.name} to {members_data[member_id]['exposure_score']}")
             await self.data_handler.save_data(members_data) # Save the data
-            print(f"saved the data")
+
+        if members_data[member_id]['exposure_status'] in ['exposed', 'infected']:
+            #print(f"Member {message.author.name} is exposed or infected.")
+            members_data[member_id]['exposure_score'] += 2 # Increment the exposure score by 2
+            #print(f"incremented exposure score for {message.author.name} to {members_data[member_id]['exposure_score']}")
+            await self.data_handler.save_data(members_data) # Save the data
+            #print(f"saved the data")
         else:
             print(f"Member {message.author.name} is clean.")
             #start tracking exposure if not already exposed or infected
             #this function is for testing only, turn off later.
-            members_data[member_id]['exposure_status'] = 'exposed'
-            members_data[member_id]['exposure_score'] = 1 # Set the exposure score to 1
+            #members_data[member_id]['exposure_status'] = 'exposed'
+            #members_data[member_id]['exposure_score'] = 1 # Set the exposure score to 1
             await self.data_handler.save_data(members_data) # Save the data
-            print(f"Member {message.author.name} is now exposed.")
+            #print(f"Member {message.author.name} is now exposed.")
 
     @commands.command() 
     async def status(self, ctx, member: discord.Member = None):
